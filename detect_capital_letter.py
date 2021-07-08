1b6bcba2241d53a885f4f3996bd601b3a8cc11da
jack <78677992+j-a-c-k-goes@users.noreply.github.com>
2021-07-09 02:14:08 +1200

    Add files via upload

---
+++ detect_capital_letter.py
@@ -0,0 +1,56 @@
+# detect a capital character in string
+# .................................................. Main Fucntion
+def detect_capital(string):
+    temp = []
+    output = None
+    punc = ["!"]
+    for character in string:
+        capital = character.upper()
+        if character == character.upper():
+            temp.append(character)
+
+    temp = "".join(temp)
+    temp = temp.replace(" ","")
+
+    for char in temp:
+
+        if char.upper() not in temp:
+            output = "there are no capital letters in\t{}".format(string)
+
+        elif char.upper() in temp:
+            output = "string\t{}\ncapital(s)\t{},'{}'".format(string,len(temp),temp)
+
+    print(output)
+    return output
+# .................................................. On Run
+if __name__ == "__main__":
+    while True:
+        string = str(input("enter a string: "))
+        detect_capital(string)
+
+        exit = input("do you want to exit? (y)es or (n)o?: ")
+        if exit == "y":
+            print("goodbye")
+            break
+            exit
+        else:
+            continue
+# .................................................. Bugs / Issues
+"""
+The loop on line 15 is not 100% correct.
+
+When there are no capital letters,
+
+program yields this error:
+
+"output referenced before assignment"
+
+* fixed partially by setting output = None (line 5)
+"""
+
+"""
+Recognizing punctaution as a capital
+
+How to fix this ? ? ? 
+
+"""
